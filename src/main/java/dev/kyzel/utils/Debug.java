5f347f14b4bde2f19a19f94f5b4efda5f2f1d4a0
luut189 <73666044+luut189@users.noreply.github.com>
2025-03-10 23:09:51 -0400

    added generic debug print

--- src/main/java/dev/kyzel/utils/Debug.java
+++ src/main/java/dev/kyzel/utils/Debug.java
@@ -14,7 +14,11 @@ public class Debug {
         System.out.println("Debug is " + (isDebug ? "on" : "off") + " now");
     }
 
-    public static void debugLog(String message) {
+    public static <T> void log(T value) {
+        log(String.valueOf(value));
+    }
+
+    public static void log(String message) {
         if (isDebug) {
             System.out.println("[DEBUG]: " + message);
         }
