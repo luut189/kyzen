5f6055b425ca06d74d2f52a3f3e8114a2b2a735c
luut189 <73666044+luut189@users.noreply.github.com>
2025-03-15 02:44:56 -0400

    created a color palette system

---
+++ src/main/java/dev/kyzel/gfx/ColorPalette.java
@@ -0,0 +1,77 @@
+package dev.kyzel.gfx;
+
+import dev.kyzel.utils.AssetManager;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class ColorPalette {
+
+    public static ColorPalette getDefaultPalette() {
+        return AssetManager.getColorPalette("assets/colors/resurrect-64.hex");
+    }
+
+    public static Vector4f getDefaultRGBA(float r, float g, float b, float a) {
+        return getDefaultPalette().getRGBA(r, g, b, a);
+    }
+
+    public static Vector4f getDefaultRGBA(float r, float g, float b) {
+        return getDefaultPalette().getRGBA(r, g, b);
+    }
+
+    public static Vector3f getDefaultRGB(float r, float g, float b) {
+        return getDefaultPalette().getRGB(r, g, b);
+    }
+
+    private final int[] PALETTE;
+
+    public ColorPalette(String path) {
+        Path file = Path.of(path);
+        try {
+            List<String> lines = Files.readAllLines(file);
+            PALETTE = new int[lines.size()];
+            for (int i = 0; i < lines.size(); i++) {
+                PALETTE[i] = Integer.parseInt(lines.get(i), 16);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Vector4f getRGBA(float r, float g, float b) {
+        return getRGBA(r, g, b, 1.0f);
+    }
+
+    public Vector4f getRGBA(float r, float g, float b, float a) {
+        return new Vector4f(getRGB(r, g, b), a);
+    }
+
+    public Vector3f getRGB(float r, float g, float b) {
+        Vector3f inputColor = new Vector3f(r, g, b);
+        Vector3f outputColor = new Vector3f();
+        double minDist = Double.MAX_VALUE;
+
+        for (int i = 0; i < PALETTE.length; i++) {
+            Vector3f color = getRGB(i);
+            double dist = inputColor.distance(color);
+            if (dist < minDist) {
+                minDist = dist;
+                outputColor = color;
+            }
+        }
+
+        return outputColor;
+    }
+
+    private Vector3f getRGB(int i) {
+        int color = PALETTE[i];
+        float r = ((color & 0xff0000) >> 16) / 255.0f;
+        float g = ((color & 0xff00) >> 8) / 255.0f;
+        float b = (color & 0xff) / 255.0f;
+        return new Vector3f(r, g, b);
+    }
+}
