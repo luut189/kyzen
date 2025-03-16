de881e16dee732aada336382afa3301d0b2c51cd
luut189 <73666044+luut189@users.noreply.github.com>
2025-03-16 17:44:42 -0400

    added debug logging's interval

--- src/main/java/dev/kyzel/kyzen/utils/Debug.java
+++ src/main/java/dev/kyzel/kyzen/utils/Debug.java
@@ -2,6 +2,7 @@ package dev.kyzel.kyzen.utils;
 
 public class Debug {
     private static boolean isDebug = false;
+    private static float currentDelta = 0f;
 
     public static void setDebug(boolean value) {
         if (isDebug == value) return;
@@ -12,6 +13,19 @@ public class Debug {
     public static void toggleDebug() {
         isDebug = !isDebug;
         System.out.println("Debug is " + (isDebug ? "on" : "off") + " now");
+    }
+
+    public static <T> void log(T value, float deltaTime) {
+        log(String.valueOf(value), deltaTime);
+    }
+
+    public static void log(String message, float deltaTime) {
+        currentDelta += deltaTime;
+        float printInterval = 1f;
+        if (isDebug && currentDelta > printInterval) {
+            System.out.println("[DEBUG]: " + message);
+            currentDelta = 0;
+        }
     }
 
     public static <T> void log(T value) {
