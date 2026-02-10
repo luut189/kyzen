b45695c58fc52767c2b2a7e850cb2c78cc844f9a
Tuong <73666044+luut189@users.noreply.github.com>
2026-02-10 17:11:51 -0500

    chore: enable macos support for lwjgl

--- src/main/java/dev/kyzel/kyzen/engine/Window.java
+++ src/main/java/dev/kyzel/kyzen/engine/Window.java
@@ -98,6 +98,10 @@ public class Window {
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
         glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 
         glfwWindow = glfwCreateWindow(width, height, title, NULL, NULL);
         if (glfwWindow == NULL) {
--- src/main/java/dev/kyzel/kyzen/gfx/Shader.java
+++ src/main/java/dev/kyzel/kyzen/gfx/Shader.java
@@ -24,11 +24,11 @@ public class Shader {
             String[] splitString = source.split("(#type)( )+([a-zA-Z]+)");
 
             int index = source.indexOf("#type") + 6;
-            int eol = source.indexOf("\r\n", index);
+            int eol = source.indexOf("\n", index);
             String firstPattern = source.substring(index, eol).trim();
 
             index = source.indexOf("#type", eol) + 6;
-            eol = source.indexOf("\r\n", index);
+            eol = source.indexOf("\n", index);
             String secondPattern = source.substring(index, eol).trim();
 
             if (firstPattern.equals("vertex")) {
